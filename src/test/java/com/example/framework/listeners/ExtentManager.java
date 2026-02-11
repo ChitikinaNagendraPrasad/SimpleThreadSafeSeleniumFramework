91e081810e360d347b519c7b083bc5cc8e5c8e8d
Nagendra <ch.nagendra@hotmail.com>
2026-02-11 21:10:56 +0530

    Modified extent-confir.xml ExtentManager.java and pom.xml to update the default logo in Extent Reports. Working.

--- src/test/java/com/example/framework/listeners/ExtentManager.java
+++ src/test/java/com/example/framework/listeners/ExtentManager.java
@@ -6,36 +6,35 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import java.io.IOException;
 import java.nio.file.Path;
 
-public final class ExtentManager
-{
-    private ExtentManager()
-    {
-    }
+public final class ExtentManager {
+
+    private ExtentManager() {}
 
     private static ExtentReports extent;
 
-    public static synchronized ExtentReports getExtent()
-    {       
-        if (extent == null)
-        {
+    public static synchronized ExtentReports getExtent() {
+
+        if (extent == null) {
+
             Path reportPath = Path.of("target", "extent-report", "ExtentReport.html");
             ExtentSparkReporter spark = new ExtentSparkReporter(reportPath.toString());
-            try
-            {
+
+            try {
                 spark.loadXMLConfig("src/test/resources/extent-config.xml");
-            } catch (IOException e)
-            {
-                // TODO Auto-generated catch block
+            } catch (IOException e) {
                 e.printStackTrace();
             }
+
             spark.config().setReportName("Automation Report - Nagendra");
             spark.config().setDocumentTitle("Automation Report");
 
             extent = new ExtentReports();
             extent.attachReporter(spark);
+
             extent.setSystemInfo("Framework", "Selenium + TestNG + POM - Nagendra");
             extent.setSystemInfo("ThreadSafe", "ThreadLocal WebDriver + ThreadLocal ExtentTest");
         }
+
         return extent;
     }
 }
